eb538a29096312c6d51b594bc30e346833f1b3ec
AWworks <dawahab@gmail.com>
2025-05-03 19:59:59 +0530

    new object

--- ts/book.ts
+++ ts/book.ts
@@ -19,4 +19,5 @@ class Book {
 }
 
 let book: Book = new Book("The Great Gatsby", "F. Scott Fitzgerald", 10.99, new Date("1925-04-10"));
+let book2: Book = new Book("Techinal Analysis", "AW Damudi", 20.99, new Date("2025-04-10"));
 console.log(book.getSummary());
