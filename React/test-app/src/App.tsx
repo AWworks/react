2bc8d8747bf1a5ef116f1f2cb1a83f82321739ad
AWworks <dawahab@gmail.com>
2025-05-21 13:37:56 +0530

    contqct

--- React/test-app/src/App.tsx
+++ React/test-app/src/App.tsx
@@ -1,11 +1,23 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './components/Home'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Contact from './components/Contact'
 
 function App() {
 
   return (
-    <>
-       Welcome
-    </>
+    <div className="container-fluid d-flex flex-column min-vh-100">
+      <Navbar />
+      <main className="container my-4 flex-grow-1">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
   )
 }
 
--- React/test-app/src/components/Header.tsx
+++ React/test-app/src/components/Header.tsx
@@ -1,12 +1,14 @@
+import { Link } from "react-router"
+
 const Header = () => {
     return (
         <header className="bg-primary text-white text-center py-5">
-        <div className="container">
-            <h1>Welcome to My Professional Website</h1>
-            <p className="lead">Simple, Clean & Responsive Bootstrap Template</p>
-            <a href="#" className="btn btn-light btn-lg">Learn More</a>
-        </div>
-    </header>
+            <div className="container">
+                <h1>Welcome to My Professional Website</h1>
+                <p className="lead">Simple, Clean & Responsive Bootstrap Template</p>
+                <Link to="/" className="btn btn-light btn-lg">Learn More</Link>
+            </div>
+        </header>
     )
 }
 
