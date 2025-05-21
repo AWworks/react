740172d7eedde92e0f395472295329d3a5009c59
AbdulWahhab <dawahab@gmail.com>
2025-05-21 21:26:53 +0530

    recipr

--- React/test-app/src/components/RecipeList.tsx
+++ React/test-app/src/components/RecipeList.tsx
@@ -11,18 +11,45 @@ interface Recipe {
 
 const RecipeList = () => {
     const [recipes, setRecipes] = useState<Recipe[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<boolean | null>(null);
+
+
+    const fetchRecipes = async () => {
+
+        try {
+            const response = await fetch("https://dummyjson.com/recipes");
+
+            if (!response.ok) {
+                throw new Error("An error occurred while fetching data.");
+            }
+
+            const data: Recipe = await response.json();
+            let recipeData: Recipe[] = data.recipes;
+            setRecipes(recipeData);
+            setLoading(false)
+        } catch (error: any) {
+            setError(error.message);
+            setLoading(false);
+        }
+    }
 
     useEffect(() => {
         fetchRecipes();
     }, []);
-    const fetchRecipes = async () => {
-        const response = await fetch("https://dummyjson.com/recipes");
-        const data = await response.json();
-        let recipeData: Recipe[] = data.recipes;
-        setRecipes(recipeData);
-    };
-    // console.log(data);
 
+    // console.log(data);
+    if (loading) {
+        return (
+            <div className="container text-center"><h1 className="text-danger">Loading...</h1></div>
+        );
+    }
+
+    if (error) {
+        return (
+            <div className="container text-center"><h1 className="text-danger fw-bold">{error}</h1></div>
+        );
+    }
     return (
         <div>
             <h1 className="text-info fw-bold">Latest Recipes</h1>
